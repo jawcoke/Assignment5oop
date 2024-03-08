8c5981ca5d23ee74b3b2ee6d96b335a96bcf58f1
wentenglin <wenteng9@gmail.com>
2024-03-08 12:23:26 -0500

    base skeleton

---
+++ src/DriverClass.java
@@ -0,0 +1,18 @@
+
+public class DriverClass {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+//------------------------------
+abstract  class Student{
+    private String name;
+    private String id;
+
+    public Student(String name, String id){
+        this.name = name;
+        this.id = id;
+    }
+}
